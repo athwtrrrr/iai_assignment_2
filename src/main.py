709ec0df47563413160659585d4a24ea22c56f68
Thu Truong <thutruong@Thus-MacBook-Air.local>
2026-04-14 11:40:55 +1000

    Update GBFS, A*, UCS, IDS

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 from fileinput import filename
 import sys
 from .graph import load_graph
-from .search import dfs_search, bfs_search   
+from .search import dfs_search, bfs_search, a_star_search, gbfs_search, ucs_search, ids_search    
 
 def main():
     if len(sys.argv) != 3:
@@ -22,15 +22,24 @@ def main():
         goal, nodes_created, path = dfs_search(graph)
     elif method == "BFS":
         goal, nodes_created, path = bfs_search(graph)
+    elif method == "A*":
+        goal, nodes_created, path = a_star_search(graph)
+    elif method == "GBFS":
+        goal, nodes_created, path = gbfs_search(graph)
+    elif method == "UCS":
+        goal, nodes_created, path = ucs_search(graph)
+    elif method == "IDS":
+        goal, nodes_created, path = ids_search(graph)
     else:
         print(f"Error: Method '{method}' not supported yet.")
         sys.exit(1)
 
+
     if goal is None:
         print(f"{filename} {method} None {nodes_created} None")
     else:
-        path_str = ' '.join(str(node) for node in path)
-        print(f"{filename} {method} {goal} {nodes_created} {path_str}")
+        path_str = '->'.join(str(node) for node in path)
+        print(f"{filename}\n {method}\n {goal}\n {nodes_created}\n {path_str}")
 
 if __name__ == "__main__":
     main()
